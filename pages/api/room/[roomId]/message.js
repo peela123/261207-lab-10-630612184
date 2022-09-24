d801e8aa6aaeaf1769a357adebe663b0fc7db8af
Peera Arunrat <peelaxv@gmail.com>
2022-09-24 19:34:43 +0700

    finish

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -4,13 +4,51 @@ import { v4 as uuidv4 } from "uuid";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB();
-  } else if (req.method === "POST") {
+
+    //get all room id
+    const roomid = req.query.roomId;
+    //get room index
+    const roomindex = rooms.findIndex((x) => {
+      x.roomId === roomid;
+    });
+    //check if room exist or not
+    if (roomindex === -1)
+      return res.status(404).json({ ok: "false", message: "Invalid room id" });
+    return res.json({ ok: "true", messages: rooms[roomindex].messages });
+  } else if (req.method === "POST") { 
     const rooms = readDB();
+    const roomid = req.query.roomId;
 
     //read request body
     const text = req.body.text;
 
     //create new id
     const newId = uuidv4();
+
+    const roomindex = rooms.findIndex((x) => {
+      x.roomId === roomid;
+    });
+
+    //case1 roomid do not exist
+    if (roomindex === -1)
+      return res.json({ ok: "false", message: "Invalid room id" });
+    //case2 invalid body
+    if (typeof text !== "string")
+      return res
+        .status(400)
+        .json({ ok: "false", message: "Invalid text input" });
+
+    //case3 valid
+    const newmessage = {
+      messageId: newId,
+      text: text
+    };
+    rooms[roomindex].messages.push(newmessage)
+    writeDB(rooms)
+
+    
+
+    return res.json({oK:"true",message:newmessage})
+
   }
 }
