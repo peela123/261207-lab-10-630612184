d23e8c68d141a197fd508f72688c1d2ad9cab2e7
Peera Arunrat <peelaxv@gmail.com>
2022-09-24 21:42:53 +0700

    f

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -4,20 +4,16 @@ import { v4 as uuidv4 } from "uuid";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB();
-
-    //get all room id
-    const roomid = req.query.roomId;
-    //get room index
-    const roomindex = rooms.findIndex((x) => {
-      x.roomId === roomid;
-    });
-    //check if room exist or not
-    if (roomindex === -1)
+    const roomId = req.query.roomId;
+    const idx = rooms.findIndex((x) => x.roomId === roomId);
+    //the room does not exist
+    if (idx === -1)
       return res.status(404).json({ ok: false, message: "Invalid room id" });
-    return res.json({ ok: true, messages: rooms[roomindex].messages });
-  } else if (req.method === "POST") { 
+
+    return res.json({ ok: true, messages: rooms[idx].messages });
+  } else if (req.method === "POST") {
     const rooms = readDB();
-    const roomid = req.query.roomId;
+    const roomId = req.query.roomId;
 
     //read request body
     const text = req.body.text;
@@ -25,30 +21,18 @@ export default function roomIdMessageRoute(req, res) {
     //create new id
     const newId = uuidv4();
 
-    const roomindex = rooms.findIndex((x) => {
-      x.roomId === roomid;
-    });
-
-    //case1 roomid do not exist
-    if (roomindex === -1)
+    const idx = rooms.findIndex((x) => x.roomId === roomId);
+    //the room does not exist
+    if (idx === -1)
       return res.status(404).json({ ok: false, message: "Invalid room id" });
-    //case2 invalid body
+    //invalid text input
     if (typeof text !== "string")
-      return res
-        .status(400)
-        .json({ ok: false, message: "Invalid text input" });
+      return res.status(400).json({ ok: false, message: "Invalid text input" });
 
-    //case3 valid both
-    const newmessage = {
-      messageId: newId,
-      text: text
-    };
-    rooms[roomindex].messages.push(newmessage)
-    writeDB(rooms)
-
-    
-
-    return res.json({oK:true,newmessage})
-
+    //valid request
+    const message = { messageId: newId, text };
+    rooms[idx].messages.push(message);
+    writeDB(rooms);
+    return res.json({ ok: true, message });
   }
 }
