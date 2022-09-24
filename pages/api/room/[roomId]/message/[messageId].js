e48a9ad27ea41eb85becdea188896e436bf7b1c6
Peera Arunrat <peelaxv@gmail.com>
2022-09-24 22:23:41 +0700

    f

--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -1,38 +1,32 @@
 import { writeDB, readDB } from "../../../../../backendLibs/dbLib";
 
 export default function roomIdMessageIdRoute(req, res) {
+  const rooms = readDB();
   //read value from URL
   const roomId = req.query.roomId;
-  const messageId = req.query.messageId;
- 
- 
-  
-  if(req.method === "DELETE")
-  {
-    const rooms = readDB()
-    const roomindex = rooms.findIndex((x) => {x.roomId === roomId})  
-    const messageindex = rooms[roomindex].messages.findIndex((x) => {x.messageId === messageId})
-    
-    
+  const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
+  const messageId = req.query.messageId
+  const messageIdx = rooms[roomIdx].messages.findIndex((x) => {x.messageId === messageId})
 
-     //case2 roomId dose not exist
-     if(roomindex === -1) return res.status(404).json({ok:false,message:"Invalid room id"})
+  if (req.method === "DELETE") {
+    const roomindex = rooms.findIndex((x) => {
+      x.roomId === roomId;
+    });
+    const messageindex = rooms[roomindex].messages.findIndex((x) => {
+      x.messageId === messageId;
+    });
 
+    // roomId dose not exist
+    if (roomIdx === -1)
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
 
-     //case3 messsage dose not exist
-     if(messageindex === -1) return res.status(404).json({ok:false,message:"Invalid message id"})
-    
-     
+    //messsage dose not exist
+    if (messageIdx === -1)
+      return res.status(404).json({ ok: false, message: "Invalid message id" });
 
-    //case1 roomId and messageId valid
-    rooms[roomindex].messages.splice(messageindex,1)
-    writeDB(rooms)
-    return res.json({ok:true})
-    
-
-   
-    
-
-     
+    // roomId and messageId valid
+    rooms[roomIdx].messages.splice(messageIdx, 1);
+    writeDB(rooms);
+    return res.json({ ok: true });
   }
 }
